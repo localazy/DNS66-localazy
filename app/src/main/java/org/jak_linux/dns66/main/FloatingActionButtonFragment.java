0b75acc5116b21132ab0be084a63f8f8a0c7c5a7
Václav Hodek <vaclav.hodek@lwi.cz>
2020-05-15 14:01:03 +0200

    Initial commit of the original project without Localazy.

---
+++ app/src/main/java/org/jak_linux/dns66/main/FloatingActionButtonFragment.java
@@ -0,0 +1,12 @@
+package org.jak_linux.dns66.main;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+/**
+ * Created by jak on 14/04/17.
+ */
+
+public interface FloatingActionButtonFragment {
+
+    void setupFloatingActionButton(FloatingActionButton fab);
+}
